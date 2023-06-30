125b7a5f162e0c0f0f90f041741d95694ea8190e
oreoluwadavid <89527998+oreoluwadavid@users.noreply.github.com>
2023-06-30 15:05:55 +0100

    Update chatbot.py
    Changed the got model to 3.5 turbo

--- video_chat_with_ChatGPT/chatbot.py
+++ video_chat_with_ChatGPT/chatbot.py
@@ -67,7 +67,7 @@ class ConversationBot:
         self.memory.clear()
         if not openai_api_key.startswith('sk-'):
             return gr.update(visible = False),state, state, "Please paste your key here !"
-        self.llm = OpenAI(temperature=0, openai_api_key=openai_api_key,model_name="gpt-4")
+        self.llm = OpenAI(temperature=0, openai_api_key=openai_api_key,model_name="gpt-3.5-turbo")
         # openai.api_base = 'https://api.openai-proxy.com/v1/'  
         self.agent = initialize_agent(
             self.tools,
